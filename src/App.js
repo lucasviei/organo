9e61c8ac317a847a91ce24d8c0f67d04926477df
Lucas Santos <lucas.santos@sefin.fortaleza.ce.gov.br>
2025-01-08 15:55:25 -0300

    Quinto modulo do projeto React Organo

--- src/App.js
+++ src/App.js
@@ -6,41 +6,47 @@ import Rodape from './componentes/Rodape';
 
 function App() {
 
-  const times = [{
-    nome: 'Front-End',
-    corPrimaria: '#82CFFA',
-    corSecundaria: '#E8F8FF',
-},
-{
-    nome: 'Data Science',
-    corPrimaria: '#A6D157',
-    corSecundaria: '#F0F8E2',
-  },
-  {
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9'
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF'
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2'
+    },
+    {
       nome: 'Devops',
       corPrimaria: '#E06B69',
-      corSecundaria: '#FDE7E8',
-  },
-  {
+      corSecundaria: '#FDE7E8'
+    },
+    {
       nome: 'UX e Design',
-      corPrimaria: '#D86EBF',
-      corSecundaria: '#FAE5F5',
-  },
-  {
+      corPrimaria: '#DB6EBF',
+      corSecundaria: '#FAE9F5'
+    },
+    {
       nome: 'Mobile',
-      corPrimaria: '#FEBA05',
-      corSecundaria: '#FFF5D9',
-  },
-  {
+      corPrimaria: '#FFBA05',
+      corSecundaria: '#FFF5D9'
+    },
+    {
       nome: 'Inovação e Gestão',
       corPrimaria: '#FF8A29',
-      corSecundaria: '#FFEEDF',
-  }]
+      corSecundaria: '#FFEEDF'
+    }
+  ]
 
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador)
     setColaboradores([...colaboradores, colaborador])
   }
 
